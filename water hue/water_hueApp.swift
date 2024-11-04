6b08cbfc4161c875850640126001bb359b86b09b
ChoSeyoung <a01075511157@gmail.com>
2024-11-04 22:09:23 +0900

    Initial Commit

---
+++ water hue/water_hueApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_hueApp.swift
+//  water hue
+//
+//  Created by sy on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct water_hueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
